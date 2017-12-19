71e0594cb4c74009416caf27777662874094958a
nichol alexander <nichol.alexander@gmail.com>
2017-12-19 18:27:27 -0500

    modified twitter-unfollow with lists.

---
+++ unfollow.rb
@@ -0,0 +1,43 @@
+require 'twitter'
+require 'pry'
+
+class Unfollow
+  def run
+    begin
+      @client = create_client
+      raise "Please create a list called 'Old Follows'" if old_follows_list.nil?
+      followings = @client.following
+
+      followings.each do |user|
+        puts "Unfollowing user #{user.screen_name} (#{user.name})"
+        # @client.add_list_member(old_follows_list, user.id)
+        @client.unfollow(user.id)
+        `echo "#{user.screen_name}" >> unfollowed_2_usernames.txt`
+        `echo "#{user.screen_name} (#{user.name}): #{user.description}" >> unfollowed_2_full_names.txt`
+        sleep 0.1
+      end
+    rescue => e
+      if e.class == Twitter::Error::TooManyRequests
+        puts "#{e} - Sleeping"
+        sleep 20
+        retry
+      end
+      binding.pry
+    end
+  end
+
+  def old_follows_list
+    @_old_follows_list ||= @client.lists.find { |a| a.name == "People To Refollow" }
+  end
+
+  def create_client
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = 'hTevf3YAcUv0mo75485CqE0qu'
+      config.consumer_secret     = 'iHXJUAf3g1gxcmYeEYUQLky2CySlllkeZ01Atw8yAvYp8NpEVV'
+      config.access_token        = '1616421-8FbWIrCayWHv5bqMAq7ZYUlOzkxbxG4Gv0fGZRiLwa'
+      config.access_token_secret = 'HPZhnBLoKNKVOBIMqWDPtCC0Nuw2ePNh9zWumiqovLXbX'
+    end
+  end
+end
+
+Unfollow.new.run
